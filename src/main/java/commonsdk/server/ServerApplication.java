2d82bd347f43b88a15726406cfa990361590ee4f
Sushil Verma <vsushil@vmware.com>
2017-11-26 20:04:37 +0530

    cors

--- src/main/java/commonsdk/server/ServerApplication.java
+++ src/main/java/commonsdk/server/ServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 public class ServerApplication {
 
 	public static void main(String[] args) {
